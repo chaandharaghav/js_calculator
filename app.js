0663b0d9b643beb60bb34091a7476dc4dd49c83d
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-07-27 20:26:37 +0530

    Add functionality to enter positive and negative number expressions

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const buttons = document.querySelectorAll("#inputPane  div:not(#numberPane)");
-console.log(buttons);
 
+// Common button functions
 const removeTransition = function (e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("clicked");
 };
-
-let clickSound = document.querySelector("audio");
 
 for (let button of buttons) {
   const audio = new Audio("click.wav");
@@ -19,3 +17,57 @@ for (let button of buttons) {
 
   button.addEventListener("transitionend", removeTransition);
 }
+
+// numbers
+const numbers = document.querySelectorAll(".numbers");
+const numberValues = [...numbers].map((number) => number.dataset.value);
+
+//operators
+const operators = document.querySelectorAll(".operators");
+const operatorValues = [...operators].map((number) => number.dataset.value);
+
+// numbers + operators
+const inputs = [...numbers, ...operators];
+
+const calculator = {
+  expression: "",
+};
+
+const changeDisplay = function (input) {
+  const lastIndex = calculator.expression.length - 1;
+  const lastElement = calculator.expression[lastIndex];
+
+  // first operator can only be negative sign or decimal point
+  if (lastIndex === -1) {
+    if (operatorValues.includes(input) && input !== "-") {
+      return "";
+    }
+  }
+
+  // an operator is not allowed if - is input previously
+  if (lastIndex === 0) {
+    if (
+      operatorValues.includes(input) &&
+      calculator.expression[lastIndex - 1] === "-"
+    ) {
+      return "";
+    }
+  }
+
+  // if incoming value and last given input are both operators, remove
+  // last given operator
+  if (operatorValues.includes(lastElement) && operatorValues.includes(input)) {
+    calculator.expression = calculator.expression.substring(0, lastIndex);
+  }
+
+  calculator.expression += input;
+  expression.innerText = calculator.expression;
+};
+
+const expression = document.querySelector("#expression");
+
+inputs.forEach((input) =>
+  input.addEventListener("click", function () {
+    changeDisplay(input.dataset.value);
+  })
+);
